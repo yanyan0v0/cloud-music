f22fe9953729c909c43fddd3c75ca4ac62dccd29
liaoyanyan <liaoyanyan@hqyatu.com>
2020-07-10 14:51:47 +0800

    初始化

---
+++ miniprogram/app.js
@@ -0,0 +1,74 @@
+//app.js
+App({
+  onLaunch: function (options) {
+    
+    this.checkUpate()
+
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        // 此处请填入环境 ID, 环境 ID 可打开云控制台查看
+        env: 'test-qfltf',
+        traceUser: true,
+      })
+    }
+    this.getOpenid()
+
+    this.globalData = {
+      playingMusicId: -1,
+      openid: -1,
+    }
+  },
+
+  onShow(options) {
+    
+  },
+
+  setPlayMusicId(musicId) {
+    this.globalData.playingMusicId = musicId
+  },
+
+  getPlayMusicId() {
+    return this.globalData.playingMusicId
+  },
+
+  getOpenid() {
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then((res) => {
+      const openid = res.result.openid
+      this.globalData.openid = openid
+      if (wx.getStorageSync(openid) == '') {
+        wx.setStorageSync(openid, [])
+      }
+    })
+  },
+
+  checkUpate() {
+    const updateManager = wx.getUpdateManager()
+    // 检测版本更新
+    updateManager.onCheckForUpdate((res) => {
+      if (res.hasUpdate) {
+        updateManager.onUpdateReady(() => {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用',
+            success(res) {
+              if (res.confirm) {
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        })
+        updateManager.onUpdateFailed(() => {
+          // 新版本下载失败
+          wx.showModal({
+            title: '已经有新版本咯~',
+            content: '请您删除当前小程序，到微信 “发现-小程序” 页，重新搜索打开呦~',
+          })
+        })
+      }
+    })
+  },
+})
